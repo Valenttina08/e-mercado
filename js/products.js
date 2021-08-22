5c25a4e196e34ea2f636a18084d8474b4e5af139
Valenttina08 <88408032+Valenttina08@users.noreply.github.com>
2021-08-22 14:45:42 -0300

    modificacion entregable 1
    se añadieron funcionalidades para el login y productos. A su vez, se cambiaron los nombres de 2 carpetas y el orden de 2 imágenes.

--- js/products.js
+++ js/products.js
@@ -1,6 +1,35 @@
-//Función que se ejecuta una vez que se haya lanzado el evento de
-//que el documento se encuentra cargado, es decir, se encuentran todos los
-//elementos HTML presentes.
+function datosDeAutos(array) {
+
+    document.getElementById("lista").innerHTML = "";
+
+    let autos = [];
+
+    for (let i = 0; i < array.length; i++) {
+
+        let producto = array[i];
+
+        autos = `Nombre:` + " " + producto.name + `<br/>` +
+                `Descripción:` + " " + producto.description + `<br/>`+
+                `Precio:` + " " + producto.cost + " " + producto.currency + `<br/>` +
+                `<img width= 400px src=  "`+ producto.imgSrc + `"`  + `<br/>` + `<hr>`
+            
+            
+        
+    
+        document.getElementById("lista").innerHTML += autos;
+    }
+}
+
+
 document.addEventListener("DOMContentLoaded", function (e) {
+
+    getJSONData(PRODUCTS_URL).then(function (resultado) {
+        if (resultado.status === "ok") {
+            productsarray = resultado.data;
+
+            datosDeAutos(productsarray);
+        }
+        
+    });
 
 });
