89cab754c6e681c96b4d7e95e47bfecd5a9db3ac
Valenttina08 <88408032+Valenttina08@users.noreply.github.com>
2021-10-02 14:20:54 -0300

    modificaciones

--- js/product-info.js
+++ js/product-info.js
@@ -4,10 +4,33 @@
 //elementos HTML presentes.
 
 
-
-var productsarray = [];
+var productsarray2 = {};
+var productsarray = {};
 var id = window.localStorage.getItem('producto');
 var comentarioListaUsu = [];
+
+
+
+function showRelacionados(autoLista) {
+    let relacion = "";
+   
+    for (let i = 0; i < autoLista.length; i++) {
+        let indice = autoLista[i];
+                    
+        
+                relacion += `Nombre:` + " " + productsarray2[indice].name + `<br/>` +
+                    `Descripción:` + " " + productsarray2[indice].description + `<br/>` +
+                    `Precio:` + " " + productsarray2[indice].cost + " " + productsarray2[indice].currency + `<br/>` +
+       `<img src="` +productsarray2[indice].imgSrc+ `" width= "300px" heigth= "300px"  >` + `<hr/>`
+       
+    }
+    
+
+    document.getElementById("relacionautos").innerHTML += relacion ;
+
+
+}
+
 
 function showProduct(producto) {
 
@@ -31,38 +54,39 @@ function ima(producto) {
     for (let i = 0; i < producto.length; i++) {
         img = producto[i];
 
-        imagenes += `
-        <div class="col-lg-3 col-md-4 col-6">
-            <div class="d-block mb-4 h-100">
-                <img class="img-fluid img-thumbnail" class="" src="` + img + `" alt="">
-            </div>
-        </div>
+        imagenes = `
+
+        
+       <div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+      <img src="`+producto[0]+`" class="d-block width="300px" height="300px">
+    </div>
+    <div class="carousel-item">
+      <img src="`+ producto[1] +`" class="d-block width="300px" height="300px">
+    </div>
+    <div class="carousel-item">
+      <img src="`+ producto[2] +`" class="d-block width="300px" height="300px">
+    </div>
+    <div class="carousel-item">
+      <img src="`+ producto[3] +`" class="d-block width="300px" height="300px">
+    </div>
+  </div>
+  <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="sr-only">Previous</span>
+  </a>
+  <a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="sr-only">Next</span>
+  </a>
+</div>
+
         `
     }
-
-
-
+    
     document.getElementById("foto").innerHTML += imagenes;
 }
-
-
-
-
-document.addEventListener("DOMContentLoaded", function (e) {
-
-    getJSONData(PRODUCT_INFO_URL_G + id + ".json").then(function (resultObj) {
-        if (resultObj.status === "ok") {
-            productsarray = resultObj.data;
-
-
-            showProduct(productsarray);
-            ima(productsarray.images);
-        }
-    });
-
-});
-
-
 
 
 
@@ -89,37 +113,11 @@ function comen(array) {
 
 
 
-        document.getElementById("comentario").innerHTML
-            += comentt;
+        document.getElementById("comentario").innerHTML += comentt;
 
     }
 }
 
-
-document.addEventListener("DOMContentLoaded", function (e) {
-
-    const url = PRODUCT_INFO_COMMENTS_URL + "comentario_" + id + ".json";
-    console.log(url)
-
-    getJSONData(PRODUCT_INFO_COMMENTS_URL + "comentario_" + id + ".json").then(function (resultado) {
-        if (resultado.status === "ok") {
-            comentarios = resultado.data;
-
-            console.log(comentarios)
-            comentt = comentarios;
-            
-            comentarioListaUsu = JSON.parse(window.localStorage.getItem('listaComenUsu'));
-            for (let i = 0; i < comentarioListaUsu.length; i++){
-                if (comentarioListaUsu[i].auto == id) {
-                    comentarios.comments.push(comentarioListaUsu[i]);
-                }
-            }
-            comen(comentarios);
-
-        }
-    });
-
-});
 
 function agregarComentario() {
     var com = document.getElementById("comentado").value;
@@ -145,7 +143,86 @@ function agregarComentario() {
     localStorage.setItem("listaComenUsu", JSON.stringify(comentarioListaUsu));
     location.reload();
 
-}
+};
+
+
+let autos = [];
+function datosDeAutos(array) {
+
+    document.getElementById("lista").innerHTML = "";
+
+    for (let i = 0; i < array.length; i++) {
+        let producto = array[i];
+
+        let auto;
+
+        {
+
+            auto = `Nombre:` + " " + producto.name + `<br/>` +
+                `Descripción:` + " " + producto.description + `<br/>` +
+                `Precio:` + " " + producto.cost + " " + producto.currency + `<br/>` +
+                `Mas Vendido:` + " " + producto.soldCount + `<br/>` + `<br/>` +
+                `<img width= "" class="card-img-top" src=  "` + producto.imgSrc + `"` + `<hr/>` + `<br>` + `<button onclick= mostrarProduct(` + producto.id + `) type= "button" class="btn btn-outline-primary"> detalle </button>` + '<br><hr><br>'
+
+        }
+
+
+        document.getElementById("lista").innerHTML += auto;
+    }
+
+
+};
+
+document.addEventListener("DOMContentLoaded", function (e) {
+
+    const url = PRODUCT_INFO_COMMENTS_URL + "comentario_" + id + ".json";
+    
+
+    getJSONData(PRODUCT_INFO_COMMENTS_URL + "comentario_" + id + ".json").then(function (resultado) {
+        if (resultado.status === "ok") {
+            comentarios = resultado.data;
+
+            comentt = comentarios;
+
+            comentarioListaUsu = JSON.parse(window.localStorage.getItem('listaComenUsu'));
+            for (let i = 0; i < comentarioListaUsu.length; i++) {
+                if (comentarioListaUsu[i].auto == id) {
+                    comentarios.comments.push(comentarioListaUsu[i]);
+                }
+            }
+            comen(comentarios);
+
+        }
+    });
+
+});
+
+
+document.addEventListener("DOMContentLoaded", function (e) {
+
+    getJSONData(PRODUCT_INFO_URL_G + id + ".json").then(function (resultObj) {
+        if (resultObj.status === "ok") {
+            productsarray = resultObj.data;
+
+
+            showProduct(productsarray);
+            ima(productsarray.images);
+        }
+
+        getJSONData(PRODUCTS_URL).then(function (result) {
+            if (resultObj.status === "ok") {
+                productsarray2 = result.data;
+
+
+                showRelacionados(productsarray.relatedProducts);
+
+            }
+        })
+    });
+
+});
+
+
 
 
 
